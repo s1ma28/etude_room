f39df24d5771931c4d9336a47684a0df383f4570
s1ma28 <s1ma28g@gmail.com>
2021-03-02 10:56:31 +0900

    フォルダ名変更 (bookapp -> etuderoom)

--- phpstudy/etuderoom/routes/web.php
+++ phpstudy/etuderoom/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Book;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // Bookモデルが持つデータを全て取得
+    $books = Book::all();
+    return view('books', ['books' => $books]);
+})->middleware('auth');   // ログインしていないとアクセスできないようにする。...便利すぎだろ笑！
+
+Route::post('/book', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $book = new Book;
+    $book->title = $request->name;
+    $book->save();
+
+    return redirect('/');
 });
+
+Route::delete('/book/{book}', function (Book $book) {
+    // 元々モデルに備わっている関数。delete
+    $book->delete();
+
+    return redirect('/');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
